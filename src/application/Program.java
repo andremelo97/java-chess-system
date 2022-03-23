eff19eb4e7f26b21b47f15a5df098b336fee33f7
Andre Melo <andre.melo9715@gmail.com>
2022-03-23 19:21:03 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,7 +11,7 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 
@@ -22,18 +22,18 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
-			} catch(ChessException e) {
+
+			} catch (ChessException e) {
 				System.out.println();
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			} catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println();
 				System.out.println(e.getMessage());
 				sc.nextLine();
